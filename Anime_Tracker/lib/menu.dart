015593515d494d30008d69d4b76bd992021c308d
Ankit <43931999+1AnkitSingh02@users.noreply.github.com>
2020-06-13 22:54:00 +0530

    Add files via upload

---
+++ Anime_Tracker/lib/menu.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Menu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+
+  }
+}
+
+Future navigateToMenu(context) async {
+  Navigator.push(context, MaterialPageRoute(builder: (context) => Menu()));
+}
